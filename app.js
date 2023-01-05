d9bbae489782ef52fbe21f2a628569f135ece773
Christian Valeri <mortlaxris@outlook.com>
2023-01-05 17:54:03 -0600

    Trying to make grid augmentive

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const container = document.getElementById('container');
 const etch = document.getElementById('etch');
+const btn = document.getElementById('btn');
+const start = () =>{
 
-//Creates 256 divs
-const createDiv = (num) =>{
-    for(let i = 0; i <= num; i++){
-        let div = document.createElement("div");
-        div.className = "sketch";
-        etch.appendChild(div);
-        console.log([i]);
-    }
-};
-createDiv(256);
+    btn.addEventListener('click', (event) => {
+        value = prompt("Please enter what grid size(?x?) do you want");
+        //return gridSum = value * value;
+    });
+    //Creates 256 divs
+    const createDiv = (num) =>{
+        for(let i = 0; i < num; i++){
+            let div = document.createElement("div");
+            div.className = "sketch";
+            etch.appendChild(div);
+            //etch.style.setProperty('grid-template-columns', 'repeat(' + value + ', 3rem)');
+        }
+    };
+    createDiv(256);
+}
+start();
